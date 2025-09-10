6dbe72f0b177c7ca258f765a1ad5fb362239bfc7
evepablo07 <23161064@itoaxaca.edu.mx>
2025-09-10 12:44:20 -0600

    Add files via upload

---
+++ IdeaProjects/login/src/login.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class login {
+    private JPanel Panelprincipal;
+    private JLabel text1;
+    private JTextField emailAddressTextField;
+    private JTextField passwordTextField;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JCheckBox rememberMeCheckBox;
+    private JButton signInButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
